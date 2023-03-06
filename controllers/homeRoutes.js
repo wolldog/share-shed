85c87e150df56ff2f6e1efd2a5eef3c6f41c2c9e
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-06 15:28:37 +1100

    M. contollers homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,55 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { Product, Category } = require("../models");
+// Import the custom middleware
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
-  res.render('index');
+// GET all products for homepage
+router.get("/", async (req, res) => {
+  try {
+    const dbProductData = await Product.findAll({
+      include: [{ model: Category}],
+    });
+    const products = dbProductData.map((product) =>
+      product.get({ plain: true })
+    );
+    res.render("homepage", {
+      products,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+// GET one product
+// Use the custom middleware before allowing the user to access the product detai
+// ** TODO - implement middleware**
+router.get('/product/:id', async (req, res) => {
+  try {
+    const dbProductData = await Product.findByPk(req.params.id);
+
+    const product = dbProductData.get({ plain: true });
+
+    res.render('product', { product, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
 });
 
 module.exports = router;
