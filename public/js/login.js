b43e10308d9fb04acbabb3ee7dcfb37986486fc9
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-06 15:33:19 +1100

    Add public/js login.js

---
+++ public/js/login.js
@@ -0,0 +1,57 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  };
+  
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+  
+    const first_name = document.querySelector('#first-name-signup').value.trim();
+    const last_name = document.querySelector('#last-name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const address = document.querySelector('#address-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+  
+  
+  
+    if (first_name && last_name && email && address && password ) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ first_name, last_name, email, address, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to sign up.');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+  
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
+  
