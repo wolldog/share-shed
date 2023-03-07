f87a04decf49e8493c7f9ffc830a2a4af385b0a2
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-03-07 16:57:12 +1100

    added booking form functionality in booking.js

---
+++ public/js/booking.js
@@ -0,0 +1,26 @@
+const bookBtn = document.getElementById('book-btn');
+const startDate = document.getElementById('start_date').value;
+const endDate = document.getElementById('end_date').value;
+const productID = document.location.search.split('/').pop();
+console.log(productID);
+
+// Add event listener to 'Book now' button
+
+bookBtn.addEventListener('click', bookBtnHandler);
+
+// BookBtnHandler function
+
+const bookBtnHandler = async (event) => {
+    const response = await fetch('/api/booking', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({ productID, startDate, endDate }),
+    });
+    
+    if (response.ok) {
+        alert('Booking successfully created!');
+      } else {
+        alert(response.statusText);
+      }
+};
+
