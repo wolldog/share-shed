7be83cbfeaba66ae1eb22ddbec06ace3dc16c1b2
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-08 20:50:41 +1100

    M. controllers\api\index.js; add listing-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 
+const listingRoutes = require('./listing-routes')
+
 router.use('/users', userRoutes);
+
+router.use('/listing', listingRoutes);
 
 module.exports = router;
