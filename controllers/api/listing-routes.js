d7a7bca872e944028d494732c8423734deaac377
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-09 08:47:34 +1100

    M. api/listing-routes, adds new listing

--- controllers/api/listing-routes.js
+++ controllers/api/listing-routes.js
@@ -1,25 +1,24 @@
 const router = require('express').Router();
-const { Product } = require('../../models');
+const { Product, User } = require('../../models');
 
 // CREATE new listing
+// ** Owner ID currently hardcoded
 router.post('/', async (req, res) => {
+  
   try {
     const dbListingData = await Product.create({
       product_name: req.body.product_name,
-      owner_id: req.session.id,
-      category_id: 1,
+      owner_id: 1,
+      category_id: req.body.category,
       product_make: req.body.make,
       product_model: req.body.model,
-      daily_rate: req.body.rate,
+      daily_rate: parseInt(req.body.rate),
       user_manual: req.body.manual,
       description: req.body.description,
       
     });
 
       res.status(200).json(dbListingData);
-      
-    
-
     } catch (err) {
     console.log(err);
     res.status(500).json(err);
