758bfd21666146ace20eb646fa90fffb7f36ef3a
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-09 23:17:25 +1100

    M. Added mapRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const bookingRoutes = require('./booking-routes');
-
+const mapRoutes = require('./map-routes')
 const listingRoutes = require('./listing-routes')
 
 
 router.use('/users', userRoutes);
 router.use('/booking', bookingRoutes);
-
+router.use('/maps', mapRoutes);
 router.use('/listing', listingRoutes);
 
 
