5b65f0f097efa00f331f7ea6c6c88e1b616ffe99
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-09 23:17:05 +1100

    Removed duplicate

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
-const helpers = require('./utils/helpers');
+
 
 
 const app = express();
