20a8fe243e933da8bce341941836bc4ab586a739
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-09 15:26:24 +1100

    M. connection.js in line with Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-// const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 // require('dotenv').config();
 
 // const sequelize = new Sequelize(
@@ -21,7 +21,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
