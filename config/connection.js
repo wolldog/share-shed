76658e927b6b01bf6540655641ef5bc54605d155
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-09 18:17:15 +1100

    Remove env.example and m. config.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,4 @@
 const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306,
-//   }
-// );
 require('dotenv').config();
 
 let sequelize;
